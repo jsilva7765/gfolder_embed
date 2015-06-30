a53602c67c89010b50556dfb9b5cdd45213feac0
jsilva7765 <joe@fullcircledesign.co>
2015-06-30 15:41:46 -0400

    Add media button js file
    This is the javascript needed to insert a shortcode for gdrive embed via a media button.

---
+++ gdrive_button.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+		$('#insert-gdrive-folder').click(function() {
+			wp.media.editor.insert('[gdrive id="InsertFolderID" style="list or grid" width="" height=""]');
+		});
+	
+});
